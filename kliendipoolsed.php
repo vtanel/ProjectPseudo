b03b366a803d3b90febf0d0501b4681616deabc1
Tanel Viira <tanel.viira@khk.ee>
2015-09-16 15:07:21 +0300

    Navigeerimatu link [Delivers #103493196]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -18,5 +18,12 @@
                 alert("Tere Maailm!!");
         }
         </script>
+    <br><br>
+    <a href="" onclick="jaame()">J&auml&aumlme siia!</a>
+        <script>
+            function jaame(){
+                alert("J"+String.fromCharCode(228,228)+"me siia...");
+            }
+        </script>
 </body>
 </html>
