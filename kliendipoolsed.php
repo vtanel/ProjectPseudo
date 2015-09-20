7be3fee864905313a04bfba1834a8a2ecd706787
Tanel Viira <tanel.viira@khk.ee>
2015-09-20 13:11:43 +0300

    5. Kass koeraks [Delivers #103494314]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -5,25 +5,40 @@
     <title>Kliendipoolsed</title>
 </head>
 <body>
-    <button onclick="tervitus()">Tere Maailm!</button>
-        <script>
-            function tervitus() {
-                alert("Tere Maailm!!");
-            }
-        </script>
-    <br><br>
-    <a href="http://www.khk.ee" onclick="terekhk()">Tere Maailm</a>
-        <script>
-            function terekhk() {
-                alert("Tere Maailm!!");
+<button onclick="tervitus()">Tere Maailm!</button>
+<script>
+    function tervitus() {
+        alert("Tere Maailm!!");
+    }
+</script>
+<br><br>
+<a href="http://www.khk.ee" onclick="terekhk()">Tere Maailm</a>
+<script>
+    function terekhk() {
+        alert("Tere Maailm!!");
+    }
+</script>
+<br><br>
+<a href="" onclick="jaame()">J&auml&aumlme siia!</a>
+<script>
+    function jaame() {
+        alert("J" + String.fromCharCode(228, 228) + "me siia...");
+    }
+</script>
+<br>
+<img
+    id="pilt"
+    src="http://1280x1024-taustapildid.free-online.gratis/pictures/Ilusaid-pilte-t%C3%B6%C3%B6laua-taustapilti-1446.jpg"
+    alt="kass" width="128px" height="136px;" onclick="muuda()">
+<script>
+    function muuda() {
+        var image = document.getElementById('pilt');
+        if (image.src.match("taustapildid")) {
+            image.src = "http://vignette1.wikia.nocookie.net/josh100lubu/images/4/40/18360-doge-doge-simple.jpg/revision/latest?cb=20150626051745";
+        } else {
+            image.src = "http://1280x1024-taustapildid.free-online.gratis/pictures/Ilusaid-pilte-t%C3%B6%C3%B6laua-taustapilti-1446.jpg";
         }
-        </script>
-    <br><br>
-    <a href="" onclick="jaame()">J&auml&aumlme siia!</a>
-        <script>
-            function jaame(){
-                alert("J"+String.fromCharCode(228,228)+"me siia...");
-            }
-        </script>
+    }
+</script>
 </body>
 </html>
