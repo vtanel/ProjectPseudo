6ad156fff8239554653a27bb1a9b7287d954940c
Tanel Viira <tanel.viira@khk.ee>
2015-09-23 14:18:43 +0300

    13. Stringid [Delivers #103980742]

---
+++ serveripoolsed.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Serveripoolsed</title>
+</head>
+<body>
+<?php
+    $eesNimi='Tanel';
+    $pereNimi='Viira';
+    echo $eesNimi.'&nbsp'.$pereNimi;
+?>
+</body>
+</html>
