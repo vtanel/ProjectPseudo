4594717fd0774f341e8e2d1d66b8c1d3badc2fb5
Tanel Viira <tanel.viira@khk.ee>
2015-10-05 12:23:59 +0300

    18. Objektid [Delivers #103980752]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -13,17 +13,26 @@ $isik=array(
     'Perenimi'=>'Viira ',
     'Vanus'=>'(21)'
 );
-foreach($isik as $key=>$value){
-    echo $value;
-};
-echo '<br><br>';
-for($i=0;$i<=count(@explode(',',$vokaalid));$i++){
-    if(substr(strtoUpper($isik['Eesnimi']),0,1) == $vokaalid[$i]){
-        echo  'Nimi algab vokaaliga.';
-    }else{
-        echo   'Nimi ei alga vokaaliga.';
-    };
-};
+class persoon{
+    public $isik=array(
+        'Eesnimi'=>'Tanel ',
+        'Perenimi'=>'Viira ',
+        'Vanus'=>21,
+        'Sugu'=>'Mees'
+    );
+    public function construkt(){
+        $this->isik["Eesnimi"] = $ees;
+        $this->isik["Perenimi"] = $pere;
+        $this->isik["Vanus"]= $vanus;
+        $this->isik["Sugu"] = $sugu;
+    }
+    public function valjasta(){
+        echo $this->isik["Eesnimi"].$this->isik["Perenimi"].'('. $this->isik["Vanus"].') '. $this->isik["Sugu"];
+    }
+
+}
+$me=new persoon();
+echo $me->valjasta();
 ?>
 </body>
 </html>
