e50b408c7c627dc10dc05bc172ce6d5ccac7ea61
Tanel Viira <tanel.viira@khk.ee>
2015-10-05 12:39:11 +0300

    19. Funktsioonid [Delivers #103980756]

--- serveripoolsed.php
+++ serveripoolsed.php
@@ -6,13 +6,24 @@
 </head>
 <body>
 <?php
-$eesnimi_algab_vokaaliga = false;
+$eesnimi_algab_vokaaliga;
 $vokaalid=array('A','E','I','O','U','Õ','Ä','Ö','Ü');
 $isik=array(
     'Eesnimi'=>'Tanel ',
     'Perenimi'=>'Viira ',
     'Vanus'=>'(21)'
 );
+foreach($isik as $key=>$value){
+        echo $value;
+};
+echo '<br><br>';
+for($i=0;$i<=count(@explode(',',$vokaalid));$i++){
+        if(substr(strtoUpper($isik['Eesnimi']),0,1) == $vokaalid[$i]){
+                echo  'Nimi algab vokaaliga.';
+    }else{
+                echo   'Nimi ei alga vokaaliga.';
+    };
+};
 class persoon{
     public $isik=array(
         'Eesnimi'=>'Tanel ',
