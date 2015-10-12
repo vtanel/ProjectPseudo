ebc7863b7c8363a5825bb62a428ead8867928b76
Tanel Viira <tanel.viira@khk.ee>
2015-10-13 01:26:03 +0300

    1429970-103980764 23. Require

--- index.php
+++ index.php
@@ -5,11 +5,13 @@
     <title>Index</title>
 </head>
 <body>
+
 <?php
 $page = isset($_GET['page']) ? $_GET['page'] : 'teremaailm';
 if(file_exists("$page")){
     require "$page.php";
 }
 ?>
+
 </body>
 </html>
