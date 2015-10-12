a742430b96483cd74ab16ca056ea7473b04beb4e
Tanel Viira <tanel.viira@khk.ee>
2015-10-13 02:07:48 +0300

    24. Require refactor (väike muutus) [Delivers #103980766]

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
     <title>Index</title>
 </head>
 <body>
+<h1>saddsaa</h1>
 <?php
 $page = isset($_GET['page']) ? $_GET['page'] : 'teremaailm';
-if(file_exists("$page")){
+if(file_exists("$page.php")){
     require "$page.php";
 }
 ?>
