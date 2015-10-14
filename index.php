960598e553797648db336333ad007aefca2ee425
Tanel Viira <tanel.viira@khk.ee>
2015-10-14 12:19:34 +0300

    28. Fake it until you make it. Made it! [Delivers #103980774]

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+$posts=array(
+    'title'=>'Man must explore, and this is exploration at its greatest',
+    'subtitle'=>'Problems look mighty small from 150 miles up',
+    'author'=>'<a href="#">Start Bootstrap</a>',
+    'date'=>'September 24, 2014'
+);
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -92,13 +106,13 @@
                 <div class="post-preview">
                     <a href="post.html">
                         <h2 class="post-title">
-                            Man must explore, and this is exploration at its greatest
+                            <?=$posts['title']?>
                         </h2>
                         <h3 class="post-subtitle">
-                            Problems look mighty small from 150 miles up
+                            <?=$posts['subtitle']?>
                         </h3>
                     </a>
-                    <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
+                    <p class="post-meta">Posted by <?=$posts['author']?> on <?=$posts['date']?></p>
                 </div>
                 <hr>
 
